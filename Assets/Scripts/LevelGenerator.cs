3ce2e24e93d7dfbb111db65f59cd6fe548465836
Rhett Haynes <rhetthay66@gmail.com>
2025-03-15 04:48:31 -0400

    Moving the platforms underneath the Player's feet as they run.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,36 +3,58 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject _chunkPrefab;
-    [SerializeField] int _startingChunksAmount = 12;
-    [SerializeField] Transform _chunkParent;
-    [SerializeField] float _chunkLength = 10f;
+    [SerializeField] GameObject _platformPrefab;
+    [SerializeField] int _startingPlatformAmount = 12;
+    [SerializeField] Transform _platformParent;
+    [SerializeField] float _platformLength = 10f;
+    [SerializeField] float _moveSpeed = 8f;
+
+    GameObject[] chunks = new GameObject[12];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < _startingChunksAmount; i++)
+        SpawnChunks();
+    }
+
+    void Update() 
+    {
+        MoveChunks();
+    }
+
+    void SpawnChunks()
+    {
+        for (int i = 0; i < _startingPlatformAmount; i++)
         {
-            float spawnPositionZ;
-
-            if (i == 0)
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else
-            {
-                // Spawn slabs in front of the player
-                spawnPositionZ = transform.position.z + (i * _chunkLength);
-            }
-
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
             Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
-            Instantiate(_chunkPrefab, chunkSpawnPos, Quaternion.identity, _chunkParent); 
+            GameObject newChunk = Instantiate(_platformPrefab, chunkSpawnPos, Quaternion.identity, _platformParent);
+            chunks[i] = newChunk;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private float CalculateSpawnPositionZ(int i)
     {
-        
+        float spawnPositionZ;
+
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            // Spawn slabs in front of the player
+            spawnPositionZ = transform.position.z + (i * _platformLength);
+        }
+
+        return spawnPositionZ;
+    }
+
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            chunks[i].transform.Translate(-transform.forward * (_moveSpeed * Time.deltaTime));
+        }
     }
 }
