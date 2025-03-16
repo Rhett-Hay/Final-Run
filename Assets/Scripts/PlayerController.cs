1aa0267793e8c0c2c696781896040ddd85cd8ec6
Rhett Haynes <rhetthay66@gmail.com>
2025-03-16 05:18:55 -0400

    Restrain Player from moving out of bounds using Clamp method.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 5f;
+    [SerializeField] float _xClamp = 3f;
+    [SerializeField] float _zClamp = 3f;
 
     Vector2 _movement;
     Rigidbody _rigidBody;
@@ -30,6 +32,8 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(_movement.x, 0f, _movement.y);
         Vector3 newPosition = currentPosition + moveDirection * (_moveSpeed * Time.fixedDeltaTime);
 
+        newPosition.x = Mathf.Clamp(newPosition.x, -_xClamp, _xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -_zClamp, _zClamp);
         _rigidBody.MovePosition(newPosition);
     }
 }
