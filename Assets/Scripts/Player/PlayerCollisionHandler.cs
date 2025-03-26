b04f279a67dc7ffb5d8d76d58a16c692742fb047
Rhett Haynes <rhetthay66@gmail.com>
2025-03-26 05:15:40 -0400

    Added a Player Stumble animation which is triggered through code when the Player collides with any fences or obstructing objects.

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator _animator;
+    [SerializeField] float _collisionCooldown = 1f;
+
+    const string _hitString = "Hit";
+    float _coolDownTimer = 0f;
+
+    void Update() 
+    {
+        _coolDownTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (_coolDownTimer < _collisionCooldown) return;
+        
+        _animator.SetTrigger(_hitString);
+        _coolDownTimer = 0f;
     }
 }
